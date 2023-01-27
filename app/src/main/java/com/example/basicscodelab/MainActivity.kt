74e3b2c1e20392021955d4229625d81d7cfec47d
shubham <thoratshubham161@gmail.com>
2023-01-27 18:43:38 +0530

    Basic Codelab

---
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.example.basicscodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BasicsCodelabTheme {
+                // A surface container using the 'background' color from the theme
+                MyApp(modifier = Modifier.fillMaxSize() )
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier , names : List<String> = listOf("World", "compose")){
+    Column(modifier = Modifier.padding(vertical = 4.dp) ) {
+        for(name in names){
+            Greeting(name = name)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp) ){
+        Column(modifier = Modifier.fillMaxWidth().padding(24.dp) ) {
+            Text(text = "Hello,")
+            Text(text = "$name!" )
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+fun DefaultPreview() {
+    BasicsCodelabTheme {
+      MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
