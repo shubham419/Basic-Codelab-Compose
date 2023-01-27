18642f0164e7b1ef67bf1fc417c0127bc8a4fe52
shubham <thoratshubham161@gmail.com>
2023-01-27 19:29:58 +0530

    Basic Codelab

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -3,18 +3,16 @@ package com.example.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.basicscodelab.ui.theme.BasicsCodelabTheme
+import androidx.compose.runtime.remember
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,11 +37,23 @@ fun MyApp(modifier: Modifier , names : List<String> = listOf("World", "compose")
 
 @Composable
 fun Greeting(name: String) {
+    val expanded = remember{ mutableStateOf(false) }
+    val extraPadding = if(expanded.value) 48.dp else 0.dp
     Surface(color = MaterialTheme.colorScheme.primary,
             modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp) ){
-        Column(modifier = Modifier.fillMaxWidth().padding(24.dp) ) {
-            Text(text = "Hello,")
-            Text(text = "$name!" )
+        Row(modifier =  Modifier.padding(24.dp) ) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
+               ) {
+                Text(text = "Hello,")
+                Text(text = "$name!" )
+        }
+            ElevatedButton(onClick = {
+                expanded.value = !expanded.value
+            }) {
+                Text( if (expanded.value)  "show more" else "Show less")
+            }
         }
     }
 }
